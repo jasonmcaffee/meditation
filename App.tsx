8ff40fa0131e0365f52e631b55bd9ab42a59c8a8
Jason McAffee <jasonlmcaffee@gmail.com>
2022-11-06 20:36:50 -0700

    audio playback of chimes working.

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React, {type PropsWithChildren, useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,13 +26,45 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TimePage from "./src/pages/TimePage";
+import TrackPlayer, {Event} from "react-native-track-player";
 
 const Stack = createNativeStackNavigator();
+
+async function playbackService(){
+    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+
+    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+}
+
+TrackPlayer.registerPlaybackService(() => playbackService);
+
+// const chimeTrack = {
+//     url: require('./assets/chime.mp3'), // Load media from the file system - no spaces allowed !!
+//     title: 'Chime',
+//     artist: 'Jason McAffee',
+//     // Load artwork from the file system:
+//     artwork: require(`./artwork/jhana icon.png`),
+//     duration: 15
+// };
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = { backgroundColor: isDarkMode ? Colors.darker : Colors.lighter, };
 
+  async function setupTrackPlayer(){
+      try{
+          await TrackPlayer.setupPlayer();
+          // await TrackPlayer.add([chimeTrack]);
+      }catch(e){
+          console.log(`error setting up trackplayer: `, e);
+      }
+  }
+  useEffect(()=>{
+      setupTrackPlayer();
+      return ()=>{
+          TrackPlayer.pause();
+      }
+  }, []);
   return (
       <NavigationContainer>
           <Stack.Navigator>
--- src/pages/TimePage.tsx
+++ src/pages/TimePage.tsx
@@ -9,13 +9,42 @@ import Button from "../common-components/Button";
 // @ts-ignore
 import * as styles from '../style/pages/time-page.scss';
 import timer from "../services/timer";
+import TrackPlayer, {Event} from 'react-native-track-player';
+
+//todo: follow setup: https://github.com/doublesymmetry/react-native-track-player/issues/1468
+//https://react-native-track-player.js.org/docs/basics/getting-started
+//https://medium.com/@bharat.tiwari/creating-an-audio-player-in-react-native-2628c4262db4
+
+async function playbackService(){
+    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+}
+
+TrackPlayer.registerPlaybackService(() => playbackService);
+
+const chimeTrack = {
+    url: require('../../assets/chime.mp3'), // Load media from the file system.  No spaces allowed!
+    title: 'Chime',
+    artist: 'Jason McAffee',
+    // Load artwork from the file system:
+    artwork: require(`../../artwork/jhana icon.png`),
+    duration: 6
+};
 
 const TimePage = () => {
     const [timeString, setTimeString] = useState(timer.getFormattedTime());
     const [startPauseText, setStartPauseText] = useState('Start');
 
-    function startPauseTimer(){
-        setStartPauseText(timer.isRunning ? 'Start' : 'Pause');
+    async function startPauseTimer(){
+        // await TrackPlayer.setupPlayer();
+        await TrackPlayer.add([chimeTrack]);
+        await TrackPlayer.setVolume(1);
+        let trackIndex = await TrackPlayer.getCurrentTrack();
+        let trackObject = await TrackPlayer.getTrack(trackIndex!);
+        console.log(`Title: ${trackObject?.title}`);
+        await TrackPlayer.play();
+
+        setStartPauseText(timer.isRunning ? `${trackObject?.title}` : 'Pause');
         timer.startPause();
     }
 
@@ -25,11 +54,18 @@ const TimePage = () => {
     }
 
     useEffect(()=>{
+        // TrackPlayer.setupPlayer();
+        // TrackPlayer.setVolume(1);
+
         const unregister = timer.onDurationUpdated((durationUpdateData) => {
             setTimeString(durationUpdateData.formattedDuration);
         });
-        return unregister;
-    })
+        return ()=>{
+            unregister();
+            TrackPlayer.pause();
+            TrackPlayer.reset();
+        }
+    }, []);
 
     return (
         <SafeAreaView style={styles.timePage}>
