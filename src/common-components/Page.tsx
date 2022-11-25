261f3a1def15e8e9a1b9617edcc4ef9c03d09359
Jason McAffee <jasonlmcaffee@gmail.com>
2022-11-24 20:11:46 -0700

    Nice page transition animation.

--- src/common-components/Page.tsx
+++ src/common-components/Page.tsx
@@ -3,12 +3,9 @@ import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
 // @ts-ignore
 import * as styles from "../style/common-components/page.scss";
 import Div from "./Div";
-import {SafeAreaView, ScrollView, Text} from "react-native";
+import {SafeAreaView, ScrollView, Text, Animated} from "react-native";
 import BottomNavigation from "./BottomNavigation";
-import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import appEventBus from "../services/appEventBus";
-type Props = NativeStackScreenProps<RootStackParamList>;
-type RootStackParamList = {};
 
 const Page: React.FC<PropsWithChildren<{pageName: string, className?: any, modal?: ReactNode}>> = ({pageName, children, className = null, modal}) => {
     const bottom = StaticSafeAreaInsets?.safeAreaInsetsBottom ?? 10;
@@ -16,17 +13,32 @@ const Page: React.FC<PropsWithChildren<{pageName: string, className?: any, modal
     const bottomStyle = {...styles.bottomNavigation, ...bottomPosition};
     const [currentPageName, setCurrentPageName] = useState(appEventBus.navigation.goToPage().get());
     const style = currentPageName == pageName ? {} : {display: 'none'};
-    // const style = {};
+    const fadeAnim = useRef(new Animated.Value(0)).current;
     const pageStyle = {...styles.page, ...style};
+    const pageContentStyle = {...styles.pageContent, opacity: fadeAnim};
+    const animationDuration = 500;
     useEffect(()=>{
-        const unreg = appEventBus.navigation.goToPage().on(setCurrentPageName);
+        console.log(`${pageName} animating to 1 in duration$ {animationDuration}`)
+        if(currentPageName == pageName){ //needed so that the animation actually fires the first time you go to the page.
+            Animated.timing(fadeAnim, {useNativeDriver: true, toValue: 1, duration: animationDuration}).start();
+        }
+        const unreg = appEventBus.navigation.goToPage().on(newPageName => {
+            setCurrentPageName(newPageName);
+            console.log(`starting animation this page: ${pageName} currentPageName ${newPageName} `, fadeAnim);
+            const toValue = newPageName == pageName ? 1 : 0;
+            const duration = newPageName == pageName ? animationDuration : 0;
+            Animated.timing(fadeAnim, {useNativeDriver: true, toValue, duration}).start();
+
+        });
+
         return () => unreg();
-    }, [])
+    }, [fadeAnim]);
+
     return <SafeAreaView style={pageStyle}>
         {modal}
-        <Div className={styles.pageContent}>
+        <Animated.View style={pageContentStyle}>
             {children}
-        </Div>
+        </Animated.View>
         <Div className={styles.bottomNavigationSpace}/>
         <BottomNavigation className={bottomStyle}/>
     </SafeAreaView>
