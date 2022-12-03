2229d25893f23441f768872b9e46e4ab63380458
Jason McAffee <jasonlmcaffee@gmail.com>
2022-12-03 12:05:09 -0700

    code cleanup and documentation.

--- src/react-utils/proxyUseState.ts
+++ src/react-utils/proxyUseState.ts
@@ -1,8 +1,5 @@
 import {Dispatch, SetStateAction, useState} from "react";
-
-type UseStateType = <S>(initialState: S | (() => S)) => [S, Dispatch<SetStateAction<S>>];
-
-
+//the return type of react's useState, which is an array of current value and a function to set the value.
 type UseStateReturnType<S> = [S, Dispatch<SetStateAction<S>>];
 
 export function pageState<TPageState extends object>(pageState: TPageState){
@@ -35,49 +32,4 @@ export function pageState<TPageState extends object>(pageState: TPageState){
     //@ts-ignore
     const proxy = new Proxy(pageState, handler);
     return proxy;
-}
-
-
-//this isn't very convenient :)
-export function proxyUseState<TValue>(val: TValue){
-    const [value, setValue] = useState(val);
-    type ProxiedObjectType = {
-        value: TValue
-    }
-    const proxiedObject: ProxiedObjectType = {
-        value: val,
-    }
-    const handler = {
-        set(obj: ProxiedObjectType, prop: string, value: TValue, receiver: any) {
-            console.log(`setting prop: ${prop} = ${value} on object: `, obj);
-            console.log(`receiver: `, receiver);
-            //@ts-ignore
-            obj[prop] = value;
-            setValue(value);
-            console.log(`obj now: `, obj);
-            //@ts-ignore
-            // return Reflect.set(...arguments);
-            return true;
-        },
-        get(obj: ProxiedObjectType, prop: string){
-            //@ts-ignore
-            return value;
-            // return obj[prop];
-        }
-    };
-    const proxy = new Proxy(proxiedObject, handler);
-    return proxy;
-}
-
-export interface IWrappedUseState<TValue>{
-    get(): TValue,
-    set(newValue: TValue): void,
-}
-
-export function wrappedUseState<TValue>(val: TValue): IWrappedUseState<TValue>{
-    const [value, setValue] = useState(val);
-    return {
-        get(){ return value; },
-        set(newVal: TValue){ setValue(newVal)}
-    }
 }
