7177167fa2d927620bfdac7b56a31e36918242d8
Jason McAffee <jasonlmcaffee@gmail.com>
2022-12-03 09:02:36 -0700

    code cleanup

--- src/common-components/Modal.tsx
+++ src/common-components/Modal.tsx
@@ -5,19 +5,21 @@ import Div from "../common-components/Div";
 import {faClose} from "@fortawesome/free-solid-svg-icons/faClose";
 import IconButton from "./IconButton";
 import {useWindowDimensions} from "react-native";
-// import {BlurView} from "react-native-blur";
 
-type Prop = React.FC<PropsWithChildren<{
+type Prop = PropsWithChildren<{
     className?: any,
     onCloseClick?: ()=> void,
     windowClassName?: any,
     showCloseButton?: boolean,
-}>>;
-const Modal: Prop = ({children, className = null, windowClassName = null, onCloseClick, showCloseButton = true}) => {
-    const screenHeight = useWindowDimensions().height; // Dimensions.get('window').height;
-    const screenWidth = useWindowDimensions().width; //Dimensions.get('window').width;
+}>;
+
+function Modal({children, className = null, windowClassName = null, onCloseClick, showCloseButton = true}: Prop){
+    //the css approach doesn't work well, so we need to get the device's height and width programmatically.
+    const screenHeight = useWindowDimensions().height;
+    const screenWidth = useWindowDimensions().width;
     const sizeStyle = {height: screenHeight, width: screenWidth};
     const style = [styles.modal, sizeStyle, className];
+
     const closeButton = showCloseButton ? <IconButton icon={faClose} onClick={onCloseClick} className={styles.modalCloseButton} iconClassName={styles.modalCloseButtonIcon} size={35}/> : null;
     return <Div className={style} onClick={onCloseClick} >
         <Div className={[styles.modalWindow, windowClassName]}>
